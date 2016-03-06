98e009074ae87064b3388db49876af4340031ebf
329720990 <dongyouweie@126.com>
2016-03-06 20:28:43 +0800

    first commit

---
+++ CordovaExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CordovaExample
+//
+//  Created by iwevon on 16/3/6.
+//  Copyright © 2016年 iwevon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CordovaExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CordovaExample
+//
+//  Created by iwevon on 16/3/6.
+//  Copyright © 2016年 iwevon. All rights reserved.
+//
+
+#import <Cordova/CDVViewController.h>
+#import <Cordova/CDVCommandDelegateImpl.h>
+#import <Cordova/CDVCommandQueue.h>
+@interface ViewController : CDVViewController
+
+@end
+
